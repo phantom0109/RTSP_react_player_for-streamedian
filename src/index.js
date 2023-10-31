6ffd804e0e9c96b2b105119ee2a0501fc025c31f
Kimura Hayate <goldenshark0702@gmail.com>
2023-10-31 17:54:47 +0900

    a. 8239

--- src/index.js
+++ src/index.js
@@ -6,10 +6,9 @@ import './index.css';
 const App = () => (
 	<div>
 		<StreamedianPlayer id="test_video">
-			{/* <source src="rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov" type="application/x-rtsp" /> */}
+			<source src="rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov" type="application/x-rtsp" />
 		</StreamedianPlayer>
 	</div>
 );
 
 render(<App />, document.getElementById('root'));
-	
